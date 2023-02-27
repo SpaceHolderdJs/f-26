1e2f31870c444c777e6b8a085bdba4a49ed9f2b9
Igor <sergienko339@gmail.com>
2023-02-27 21:13:07 +0200

    arrays in JS (reduce)

--- Site/arrays.js
+++ Site/arrays.js
@@ -138,65 +138,168 @@
 
 // console.log(fn(arr), "reverse");
 
-const arr = [1, 2, 3, 4, 5];
+// const arr = [1, 2, 3, 4, 5];
 
-const names = ["Igor", "Oleg", "Olena"];
-const ages = [{ age: 10 }, { age: 20 }, { age: 11 }];
+// const names = ["Igor", "Oleg", "Olena"];
+// const ages = [{ age: 10 }, { age: 20 }, { age: 11 }];
 
-const arrOfString = arr.map((el, i) => el + 10);
+// const arrOfString = arr.map((el, i) => el + 10);
 
-console.log(arrOfString, "result");
+// console.log(arrOfString, "result");
 
-const updatedAges = ages.map((object, i) => {
-    object.age += 1;
-    object.name = names[i];
-    return object;
-});
+// const updatedAges = ages.map((object, i) => {
+//     object.age += 1;
+//     object.name = names[i];
+//     return object;
+// });
 
-const updatedNames = names.map((name, i) => {
-  return { name: name, age: ages[i].age };
-});
+// const updatedNames = names.map((name, i) => {
+//   return { name: name, age: ages[i].age };
+// });
 
-console.log(updatedNames, "names");
+// console.log(updatedNames, "names");
 
 // 4. Написати функцію що приймає масив рядків та повертає масив з number на базі масиву з рядками
 // ["1", "2", "3"] => [1, 2, 3]
 
-console.log(
-  ["1", "2", "3"].map((el) => +el),
-  "result"
-);
+// console.log(
+//   ["1", "2", "3"].map((el) => +el),
+//   "result"
+// );
 
-//filter
+// //filter
 
-//mutable
+// //mutable
 
-const arr2 = ["1", 1, "Hello", 100];
+// const arr2 = ["1", 1, "Hello", 100];
 
-const result = arr2.filter((el) => typeof el === "string");
+// const result = arr2.filter((el) => typeof el === "string");
 
-console.log(result, "result");
+// console.log(result, "result");
 
 // 5. Дано масив const arr = [{age: 10}, {age: 22}, {age: 5}];
 // 6. Додати до масиву що повернув вам filter до поля age кожного об`єкта 20;
 // 7. Перегорніть отриманий масив
 
-const users = [
-  { name: "Oleg", age: 22, city: "Kharkiv" },
-  { name: "Oleg", age: 22, city: "Kyiv" },
-  { name: "Oleg", age: 40, city: "Poltava" },
-  { name: "Oleg", age: 30, city: "Poltava" },
-];
+// const users = [
+//   { name: "Oleg", age: 22, city: "Kharkiv" },
+//   { name: "Oleg", age: 22, city: "Kyiv" },
+//   { name: "Oleg", age: 40, city: "Poltava" },
+//   { name: "Oleg", age: 30, city: "Poltava" },
+// ];
 
 // 1. Поверніть масив лише тих користувачів, що мають поле city
 // 2. На базі попреднього масиву визначте користувача з найбільшим віком
 
-let biggestAge = users[0];
+// let biggestAge = users[0];
 
-users.forEach((object) => {
-    if (object.age > biggestAge.age) {
-        biggestAge = object;
-    }
-});
+// users.forEach((object) => {
+//     if (object.age > biggestAge.age) {
+//         biggestAge = object;
+//     }
+// });
 
-console.log(biggestAge, "biggest age");
+// console.log(biggestAge, "biggest age");
+
+// every, some, reduce, filter, this, [...arr]
+
+// const arr = [1,2,3,4,5];
+
+// const result = arr.every((e) => typeof e === "number");
+// console.log(result, "result of every");
+
+// const result2 = arr.some((e, i, arr) => e === 6);
+// console.log(result2, "result of some");
+
+// 1. всі а у об`єктах є числами (number)
+// 2. всі а у об`єктах мають довжину 2
+// 3. деякі а у об`єктах мають у складі цифру 1
+
+// const arr2 = [{a: 10}, {a: 20}, {a: 30}];
+
+// Array.prototype.myEvery = function (fn) {
+//   let result = true;
+
+//   for (let i = 0; i < this.length; i = i + 1) {
+//     const callBackResult = fn(this[i], i, this); //criteria
+//     if (!callBackResult) {
+//       result = false;
+//     }
+//   }
+
+//   return result;
+// }
+
+// console.log([1,2,3,4,5].myEvery((e) => typeof e === "number"), "myEvery");
+
+// el, i, arr
+// acc, el, i, arr
+
+// const result = [1, 2, 3, 4, 5].reduce((acc, el) => (acc = acc + el));
+// console.log(result, "result");
+
+// const data = [
+//   { name: "Kyiv", population: 130 },
+//   { name: "Lviv", population: 230 },
+//   { name: "Kharkiv", population: 630 },
+//   { name: "Poltava", population: 430 },
+// ];
+
+// const result = data.reduce((acc, obj) => {
+//   acc.push({...obj, population: obj.name.length});
+//   return acc;
+// }, []);
+
+// const result = data.reduce((acc, obj, i) => {
+//   acc[i] = obj;
+//   return acc;
+// }, {});
+
+// console.log("result", result);
+
+const data = [
+ { name: "Kyiv", population: 130 },
+ { name: "Lviv", population: 230 },
+ { name: "Kharkiv", population: 630 },
+ { name: "Poltava", population: 430 },
+];
+
+// Object.keys
+// Object.values
+
+//1 Повернути масив на базі data в тому вигляді, що він був на початку
+
+// const result = Object.keys(data).reduce((acc, el) => {
+//   acc.push(data[el]);
+//   return acc;
+// }, []);
+
+// console.log(result, "result");
+
+// const result = data.filter((obj) => obj.population > 430);
+
+// console.log(result, "result");
+
+// Array.prototype.myFilter = function (fn) {
+//   const result = [];
+
+//   for (let i = 0; i < this.length; i = i + 1) {
+//    const callBackResult = fn(this[i], i, this);
+
+//    if (callBackResult) {
+//     result.push(this[i]);
+//    }
+//   }
+
+//   return result;
+// }
+
+const result2 = [1, "Hello", 2, "World"].reduce((acc, el) => {
+  if (typeof el === "string") {
+    acc.push(el);
+  }
+
+  return acc;
+}, []);
+
+console.log(result2, "result2");
